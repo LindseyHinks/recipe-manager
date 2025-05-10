692deed1075dae1ea9a195afc1ce3954b649b52e
Lindsey Hinks <lindseyhinks@outlook.com>
2025-05-10 14:45:09 +0100

    Implement sorting of recipes by title and missing ingredients

--- frontend/src/components/Recipes.jsx
+++ frontend/src/components/Recipes.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import { deleteRecipe, getRecipes } from '../services/recipes';
-import { Button, Alert, Card, Badge } from 'react-bootstrap';
+import { Button, Alert, Card, Badge, Form, Row, Col } from 'react-bootstrap';
 import AddRecipeModal from './AddRecipeModal';
 import { getCupboard } from '../services/cupboard';
 import RecipeModal from './RecipeModal';
@@ -18,6 +18,8 @@ export default function Recipes() {
     const [addRecipe, setAddRecipe] = useState(false);
     const [recipeToEdit, setRecipeToEdit] = useState(null);
     const [recipeToShow, setRecipeToShow] = useState(null);
+    const [sortBy, setSortBy] = useState("title");
+    const [sortedRecipes, setSortedRecipes] = useState([]);
 
     useEffect(() => {
         async function loadRecipes() {
@@ -57,6 +59,23 @@ export default function Recipes() {
         loadRecipes();
     }, []);
 
+    useEffect(() => {
+        // sort alphabetically by title or the number of missing ingredients in the recipe
+        const sorted = [...recipes].sort((a, b) => {
+            if (sortBy === 'title') {
+                return a.title.localeCompare(b.title);
+            }
+            if (sortBy === 'missing') {
+                // get number of missing ingredients in each recipe
+                const aCount = a.ingredients.reduce((acc, cur) => cur.missing ? ++acc : acc, 0);
+                const bCount = b.ingredients.reduce((acc, cur) => cur.missing ? ++acc : acc, 0);
+                return aCount - bCount;
+            }
+            return 0;
+        });
+        setSortedRecipes(sorted);
+    }, [sortBy, recipes]);
+
     /**
      * Handles the deletion of the specific recipe. Calls the API
      * endpoint to delete it and updated the recipes object.
@@ -86,10 +105,25 @@ export default function Recipes() {
     return <div className='m-3'>
         <div className={`d-flex align-items-center ${error ? 'justify-content-between' : 'justify-content-end'}`}>
             {error && <Alert variant="danger" className='w-fit-content p-2 mt-2'>{error}</Alert>}
-            <Button variant="dark" className='mb-2' onClick={() => setAddRecipe(true)}>Add recipe</Button>
+            <div className='d-flex mb-2'>
+                <Form.Group as={Row}>
+                    <Col sm="auto" className='mt-2 pe-0 fw-bold'>
+                        Sort by:
+                    </Col>
+                    <Col sm="auto">
+                        <Form.Select onChange={(e) => setSortBy(e.target.value)}>
+                            <option value="title">Title</option>
+                            <option value="missing">Missing Ingredients</option>
+                        </Form.Select>
+                    </Col>
+                    <Col sm="auto">
+                        <Button variant="dark" onClick={() => setAddRecipe(true)}>Add recipe</Button>                    
+                    </Col>
+                </Form.Group>
+            </div>
         </div>
         <div className='row'>
-            {recipes.map(recipe => (
+            {sortedRecipes.map(recipe => (
                 <div className='col-md-4 mb-4' key={recipe.id}>
                     <Card onClick={() => setRecipeToShow(recipe)} className='shadow-sm transition card-as-button'>
                         <Card.Img variant='top' src={'/recipe.png'} alt={recipe.title} className='w-50 align-self-center' />
@@ -106,8 +140,8 @@ export default function Recipes() {
                                 <span className='text-success fw-bold'>You have all the ingredients!</span>
                             )}
                             <div className='d-flex justify-content-end mt-2'>
-                                <Button size="sm" variant="outline-secondary" onClick={() => setRecipeToEdit(recipe)}>Edit</Button>
-                                <Button size="sm" variant="outline-danger" className='ms-2' onClick={() => handleDeleteRecipe(recipe.id)}>Delete</Button>
+                                <Button size="sm" variant="outline-secondary" onClick={(e) => {e.stopPropagation(); setRecipeToEdit(recipe)}}>Edit</Button>
+                                <Button size="sm" variant="outline-danger" className='ms-2' onClick={(e) => {e.stopPropagation(); handleDeleteRecipe(recipe.id)}}>Delete</Button>
                             </div>
                         </Card.Body>
                     </Card>
@@ -115,7 +149,7 @@ export default function Recipes() {
             ))}
         </div>
         {recipeToShow && <RecipeModal onHide={() => setRecipeToShow(null)} recipe={recipeToShow} />}
-        {addRecipe && <AddRecipeModal onHide={() => {setAddRecipe(false); setRecipeToShow(null)}} setRecipes={setRecipes} cupboard={cupboard} />}
-        {recipeToEdit && <AddRecipeModal onHide={() => {setRecipeToEdit(null); setRecipeToShow(null)}} setRecipes={setRecipes} cupboard={cupboard} data={recipeToEdit} />}
+        {addRecipe && <AddRecipeModal onHide={() => setAddRecipe(false)} setRecipes={setRecipes} cupboard={cupboard} />}
+        {recipeToEdit && <AddRecipeModal onHide={() => {setRecipeToEdit(null)}} setRecipes={setRecipes} cupboard={cupboard} data={recipeToEdit} />}
     </div>;
 }
